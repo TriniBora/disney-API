8668a6461ff873fe26f08fdcbc623c930ead87bc
Trinidad Boragini <mtboragini@gmail.com>
2022-02-11 17:39:03 -0300

    Created POST, PUT, DELETE Movie

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -6,9 +6,7 @@ const findMovies = async (req, res) => {
     const queryKeys = Object.keys(req.query);
     // Only allowed to filter movies by title, genre and sort by creation date. If any other query parameter is passed,
     // an error message is returned
-    if (
-      queryKeys.every((key) => ["title", "genre", "creationDate"].includes(key))
-    ) {
+    if (queryKeys.every((key) => ["title", "genre", "order"].includes(key))) {
       const movies = await movieService.findMoviesService(req.query);
       res.status(200).json({
         status: 200,
@@ -31,14 +29,91 @@ const findMovies = async (req, res) => {
     res.status(500).json(error);
   }
 };
-const findMovieById = async (req, res) => {};
-const createMovie = async (req, res) => {};
-const updateMovie = async (req, res) => {};
-const deleteMovie = async (req, res) => {};
+
+// This function is used to find a movie/serie by ID, passed in as a URL parameter
+// If the movie/serie is not found, an error message is returned
+const findMovieById = async (req, res) => {
+  try {
+    const movie = await movieService.findMovieByIdService(req.params.id);
+    res.status(200).json({
+      status: 200,
+      data: movie,
+      message: `${
+        movie.length === 1
+          ? "Movie/serie retrieved successfully."
+          : "There is no exists any movie/serie with this ID in the database."
+      }`,
+    });
+  } catch (error) {
+    res.status(error.code).json({
+      status: error.code,
+      data: null,
+      message: error.message,
+    });
+  }
+};
+
+const findGenreMovies = async (req, res) => {};
+
+// This function is used to create a new movie/serie
+// If thw movie/serie data is not valid, an error message is returned ---------- AGREGAR VALIDATE MODEL!!
+const createMovie = async (req, res) => {
+  try {
+    await movieService.createMovieService(req.body);
+    res.status(201).json({
+      status: 201,
+      data: null,
+      message: "Movie/serie created successfully",
+    });
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+};
+
+// This function is used to update an existing movie/serie, given its ID as a URL parameter and its new data as a request body
+// If the movie/serie is not found, an error message is returned
+// If the movie/serie data is not valid, an error message is returned ---------- AGREGAR VALIDATE MODEL!!
+const updateMovie = async (req, res) => {
+  try {
+    await movieService.updateMovieService(req.body, req.params.id);
+    res.status(200).json({
+      status: 200,
+      data: null,
+      message: "Movie/serie updated successfully",
+    });
+  } catch (error) {
+    res.status(error.code).json({
+      status: error.code,
+      data: null,
+      message: error.message,
+    });
+  }
+};
+
+// This function is used to delete a movie/serie from the database
+// If the movie/serie ID is invalid, an error message is returned
+const deleteMovie = async (req, res) => {
+  try {
+    await movieService.deleteMovieService(req.params.id);
+    res.status(200).json({
+      status: 200,
+      data: null,
+      message: "Movie/serie deleted successfully",
+    });
+  } catch (error) {
+    res.status(error.code).json({
+      status: error.code,
+      data: null,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   findMovies,
   findMovieById,
+  findGenreMovies,
   createMovie,
   updateMovie,
   deleteMovie,
--- services/MovieService.js
+++ services/MovieService.js
@@ -10,8 +10,25 @@ const findMoviesService = async (query) => {
   // the exactly given genre in case that the query parameters are passed
   if (Object.keys(query).includes("title")) {
     const movies = await movieModel.findAll({
-      //Includes the Op.like sequilize operator to match the name partially or exactly
+      //Includes the Op.like sequelize operator to match the name partially or exactly
       where: { ...query, title: { [Op.like]: `%${query.title}%` } },
+    });
+    return movies;
+  } else if (Object.keys(query).includes("order")) {
+    const order = query.order.toUpperCase();
+
+    // If the order is diferent to ASC or DESC, an error message is returned
+    if (order !== "ASC" && order !== "DESC") {
+      throw { code: 400, message: "Order must be ASC or DESC" };
+    }
+
+    //Removes order key because this is not a column in the database table "movies"
+    delete query["order"];
+
+    const movies = await movieModel.findAll({
+      //Includes the "order" sequelize option to sort the movies/series by creation date, ascending or descending
+      where: query,
+      order: [["creationDate", order]],
     });
     return movies;
   } else {
@@ -22,6 +39,71 @@ const findMoviesService = async (query) => {
   }
 };
 
+// This function return the movie/serie with the given id stored in the database
+// If the movie/serie is not found, an error message is returned
+const findMovieByIdService = async (id) => {
+  const movie = await movieModel.findAll({
+    where: {
+      id: id,
+    },
+  });
+  if (movie.length === 0) {
+    throw { code: 400, message: "Invalid movie/serie id" };
+  }
+  return movie;
+};
+
+// This function inserts a new movie/serie in the database
+const createMovieService = async (payload) => {
+  const { title, rate, creationDate, image } = payload;
+  const movie = await movieModel.create({
+    title: title,
+    rate: rate,
+    creationDate: creationDate,
+    image: image,
+  });
+  return movie;
+};
+
+// This function updates only the modified data of the movie/serie with the given id stored in the database
+const updateMovieService = async (payload, id) => {
+  //Destructuring the payload
+  const { title, rate, creationDate, image } = payload;
+
+  // Checks if the movie/serie exists in the database, if not found, throws an error
+  await findMovieByIdService(id);
+  const movieUpdated = await movieModel.update(
+    {
+      title: title || movie.title,
+      rate: rate || movie.rate,
+      creationDate: creationDate || movie.creationDate,
+      image: image || movie.image,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+  return movieUpdated;
+};
+
+// This function deletes the movie/serie with the given id stored in the database
+const deleteMovieService = async (id) => {
+  // Checks if the movie/serie exists in the database, if not found, throws an error
+  await findMovieByIdService(id);
+  const movieDeleted = await movieModel.destroy({
+    where: {
+      id: id,
+    },
+  });
+  return movieDeleted;
+};
+
 module.exports = {
   findMoviesService,
+  findMovieByIdService,
+  createMovieService,
+  updateMovieService,
+  deleteMovieService,
 };
